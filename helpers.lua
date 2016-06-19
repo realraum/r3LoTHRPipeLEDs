00040459eea45657b5500f475304c0abfbe7a7d4
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2016-06-19 02:42:37 +0200

    helpers: Export pp

--- helpers.lua
+++ helpers.lua
@@ -1,2 +1,4 @@
 -- debug stuff
 function pp(t) local tt={} local i=0 for k,v in pairs(t) do i=i+1 tt[i]=tostring(k) .. " = " .. tostring(v) end  table.sort(tt) print(table.concat(tt,"\n")) end
+
+setglobal("pp", pp)
