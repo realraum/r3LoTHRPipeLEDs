3d49a6194999dcaad340de51d77c5c3981bea115
Bernhard Tittelbach <bernhard@tittelbach.org>
2016-06-24 01:35:18 +0200

    bit faster and check args

--- patt_rainbow.lua
+++ patt_rainbow.lua
@@ -3,12 +3,15 @@ local PIXELS     = 30*5
 
 local rainbow_index = 1
 local function run(wsbuf, rainbow_speed)
+  if not (type(rainbow_speed) == "number" and rainbow_speed > 0) then
+    rainbow_speed = 1
+  end
   for pixel = 1, PIXELS do
 --    tmr.wdclr()
     wsbuf:set(pixel, hue2rgb((pixel * rainbow_speed + rainbow_index) % 256))
   end
   rainbow_index = (rainbow_index + 1) % 256
-  return 100
+  return 70
 end
 
 return run
