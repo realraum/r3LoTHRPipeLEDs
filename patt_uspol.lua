19d12ccbb49b6b16fc5efe5fe38b61acae0911e3
Bernhard Tittelbach <bernhard@tittelbach.org>
2016-06-24 01:34:15 +0200

    works now :-)

--- patt_uspol.lua
+++ patt_uspol.lua
@@ -2,25 +2,30 @@ local PIXELS = 150
 local phase = 0
 local center_red = PIXELS * 2 / 3
 local center_blue = PIXELS * 1 / 3
-local maxamp = PIXELS / 3 - (PIXELS/30)
+local maxamp = PIXELS / 6 - (PIXELS/30)
 local SIN = lut.sin
 
 local function run(wsbuf)
-    fill(wsbuf,0,0,0)
-    local set = wsbuf.set
-    local amp_red = maxamp * (SIN[phase] - 127) / 256
+    wsbuf:fill(0,0,0)
+    local pphase = phase * 5
+    local sin_red = SIN[pphase % 256]
+    local sin_blue = SIN[(pphase+32) % 256]
+    pphase = nil
+    local amp_red = maxamp * sin_red / 256
+    local amp_blue = maxamp * sin_blue / 256
+
     for i = 0, amp_red do
-    	set(wsbuf, center_red+i,0,amp_red*3,0)
-    	set(wsbuf, center_red-i,0,amp_red*3,0)
+    	wsbuf:set(center_red+i,0,255 - sin_red + amp_red - i,0)
+    	wsbuf:set(center_red-i,0,255 - sin_red + amp_red - i,0)
     end
-    local amp_blue = maxamp * (SIN[(phase+16) % 256] - 127)  / 256
+
     for i = 0, amp_blue do
-    	set(wsbuf, center_blue+i,0,0,amp_blue*3)
-    	set(wsbuf, center_blue-i,0,0,amp_blue*3)
+    	wsbuf:set(center_blue+i,0,0,255 - sin_blue + amp_blue - i)
+    	wsbuf:set(center_blue-i,0,0,255 - sin_blue + amp_blue - i)
     end
-    
+
     phase = (phase + 1) % 256
-    return 60
+    return 40
 end
 
 return run
