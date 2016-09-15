7c9d980a1163144d9beaa24133c0b604e6a0a33d
Bernhard Tittelbach <bernhard@tittelbach.org>
2016-09-15 22:32:24 +0200

    eliminate main2.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,23 @@
-return dofile("main2.lua")
+local function setglobal(k,v) rawset(_G, k, v) end
+local function import(m, ...)
+    local t = assert(loadfile(m .. ".lua"))(...)
+    if t then
+        setglobal(m, t)
+    end
+end
+setglobal("setglobal", setglobal)
+setglobal("import", import)
+
+import "startup"
+import "telnet"
+telnet.open()
+
+-- a global under the same name will be registered by safe.lua
+local function errorLog(s)
+    print(s)
+    setglobal("LAST_ERROR", s)
+    return s
+end
+
+-- load the rest, safely
+xpcall(function() import "loader" end, errorLog)
