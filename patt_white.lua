e830a6f6468b9a90a53071dd236ea16ef67fe33e
Bernhard Tittelbach <bernhard@tittelbach.org>
2016-10-30 00:34:51 +0200

    check arguments

--- patt_white.lua
+++ patt_white.lua
@@ -1,6 +1,10 @@
 local function run(wsbuf, bright_percent)
-    if bright_percent > 100 then bright_percent = 100 end
-    if bright_percent < 0 then bright_percent = 0 end
+	if type(bright_percent) == "number" then
+		if bright_percent > 100 then bright_percent = 100 end
+		if bright_percent < 0 then bright_percent = 0 end
+	else
+		bright_percent = 100
+	end
     wsbuf:fill(255*bright_percent/100, 255*bright_percent/100, 255*bright_percent/100)
     return 1000
 end
