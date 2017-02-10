0b45a4adbe1ee0841eb818db0af11aac274400eb
Bernhard Tittelbach <bernhard@tittelbach.org>
2017-02-10 23:38:12 +0100

    bugfix für spot anzahl änderung

--- patt_movingspots.lua
+++ patt_movingspots.lua
@@ -5,6 +5,7 @@ local function run(wsbuf, p, spots_count)
     spots_count = 1
   end
   local spots_distance = wsbuf:size()/spots_count
+  spot_index = (spot_index + 1) % spots_distance
   wsbuf:fade(2,ws2812.FADE_OUT)
   local pos = 0
   for nspot = 0, spots_count -1 do
@@ -12,7 +13,6 @@ local function run(wsbuf, p, spots_count)
     wsbuf:set(pos, 255 ,255, 255)
     if pos < wsbuf:size() then wsbuf:set(pos+1, 64 ,64, 64) end
   end
-  spot_index = (spot_index + 1) % spots_distance
   return 20 + ((200 * (255 - p.speed)) / 255)
 end
 
