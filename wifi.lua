e2a4924c8940bffa72d76d2de81885ce2d8e6d00
Bernhard Tittelbach <bernhard@tittelbach.org>
2019-02-10 04:31:14 +0100

    support static ips

--- wifi.lua
+++ wifi.lua
@@ -1,6 +1,7 @@
 wifi.setmode(wifi.STATION)
 wifi.sta.config(unpack(CONFIG.wifi))
 wifi.sta.autoconnect(1)
+if CONFIG.ipconf then wifi.sta.setip(CONFIG.ipconf) end
 
 --connect to WIFI as configured in init.lua
 wifi.sta.connect()
