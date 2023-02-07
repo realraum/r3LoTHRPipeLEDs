ffa385af9631b0603bd75b0338d2cbc9ff53fbf2
Bernhard Tittelbach <bernhard@tittelbach.org>
2023-02-08 00:22:00 +0100

    linebreaks

--- wifi.lua
+++ wifi.lua
@@ -1,18 +1,18 @@
 wifi.setmode(wifi.STATION)
-wifi.sta.config(unpack(CONFIG.wifi))
+wifi.sta.config(CONFIG.wifi)
 wifi.sta.autoconnect(1)
 if CONFIG.ipconf then wifi.sta.setip(CONFIG.ipconf) end
 
 --connect to WIFI as configured in init.lua
-wifi.sta.connect()
+--wifi.sta.connect()
 
 --wait until connected to WIFI
--- FIXME: get rid of timer IDs
-tmr.alarm(0, 1000, 1, function()
+local wifiwaittmr = tmr.create()
+wifiwaittmr:alarm(1000, tmr.ALARM_AUTO, function()
    if wifi.sta.getip() == nil then
       print("Connecting to AP...")
    else
       print('IP: ',wifi.sta.getip())
-      tmr.stop(0)
+      wifiwaittmr:stop(0)
    end
 end)
